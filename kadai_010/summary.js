80c440901ae8cd9fc66554aaf99245b42146021e
natsuki33373 <162120853+natsuki33373@users.noreply.github.com>
2024-04-24 20:20:50 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,16 @@
+$('#change-color').click(function() {
+    $('#target').css('color', 'red');
+  });
+  
+  $('#change-text').click(function() {
+    $('#target').text('Hello!');
+  });
+  
+  $('#fade-out').click(function() {
+    $('#target').fadeOut(2000);
+  });
+  
+  $('#fade-in').click(function() {
+    $('#target').fadeIn(2000);
+  });
+  
